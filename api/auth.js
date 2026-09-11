c8de4bde242ad95cad8d0e966c7b29393cd0db20
Darren Bardelas <darrenjohnlibarrabardelas@gmail.com>
2026-09-11 14:20:47 +0800

    Credentials

--- api/auth.js
+++ api/auth.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
   const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    'https://sfo-system.vercel.app/api/oauth-callback' // Must match Google Console exactly
+    '   ' // Must match Google Console exactly
   );
 
   const url = oauth2Client.generateAuthUrl({
